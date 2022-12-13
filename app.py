d2def1a7483d27be3a771ee72a87ed031a05a616
JuanChopin <elcorreodejuanitoalzate@gmail.com>
2022-12-13 01:36:04 -0500

    Ficheros backend Python agregados

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/pizza", methods = ['POST'])
+def prueba ():
+    nombre = request.form.get("nombreCliente")
+    apellido = request.form.get("apellidoCliente")
+    print(nombre, apellido)
+    
+    with open("pedidos.txt", "a", encoding = "utf-8") as file:
+        file.write(nombre + " " + apellido + "\n")
+        file.close()
+    return redirect("http://localhost/solicita_pedido.html", code=302)
