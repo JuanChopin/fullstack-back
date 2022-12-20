dce0c83682856e67a1335c1637b0643d85097c23
JuanChopin <elcorreodejuanitoalzate@gmail.com>
2022-12-20 18:21:38 -0500

    add docstrings

--- test_persistencia.py
+++ test_persistencia.py
@@ -1,11 +1,12 @@
+"""Pruebas persistencia"""
 import persistencia
 
-def test_guardar_pedido(): 
-    with open("pedidos.txt","w+", encoding="utf-8") as file:
+def test_guardar_pedido():
+    """Prueba general"""
+    with open("pedidos.txt","w+",encoding="utf-8") as file:
         persistencia.guardar_pedido("Pedro", "Gil de Diego")
         persistencia.guardar_pedido("Michael","Jordan")
         firstline = file.readline()
         secondline = file.readline()
         assert firstline == "-Pedro Gil de Diego\n"
         assert secondline == "-Michael Jordan\n"
-
