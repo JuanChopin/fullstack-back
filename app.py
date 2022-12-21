c536c93b2c657ddadaa450b6e9d6d9c4131dba36
JuanChopin <elcorreodejuanitoalzate@gmail.com>
2022-12-20 19:24:51 -0500

    Correccion errores Pylint

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+"""Pizza full stack Backend"""
 from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/pizza", methods = ['POST'])
 def prueba ():
+    """Funcion que se ejecuta al invocarse /pizza"""
     nombre = request.form.get("nombreCliente")
     apellido = request.form.get("apellidoCliente")
     print(nombre, apellido)
-    
     with open("pedidos.txt", "a", encoding = "utf-8") as file:
         file.write(nombre + " " + apellido + "\n")
         file.close()
--- persistencia.py
+++ persistencia.py
@@ -1,4 +1,6 @@
+"""Guardar pedido"""
 def guardar_pedido(nombre, apellido):
+    """Esta funcion guarda un pedido en un archivo txt."""
     with open("pedidos.txt","a", encoding = "utf-8") as file:
         file.write(nombre + " " + apellido + "\n")
         file.close()
