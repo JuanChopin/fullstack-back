00d217e74c6d02fe1c7969127f469ac0f6b7a7f8
JuanChopin <elcorreodejuanitoalzate@gmail.com>
2022-12-20 19:29:00 -0500

    Adición del guion para subsanar errores de Pytest

--- persistencia.py
+++ persistencia.py
@@ -2,5 +2,5 @@
 def guardar_pedido(nombre, apellido):
     """Esta funcion guarda un pedido en un archivo txt."""
     with open("pedidos.txt","a", encoding = "utf-8") as file:
-        file.write(nombre + " " + apellido + "\n")
+        file.write("-" + nombre + " " + apellido + "\n")
         file.close()
